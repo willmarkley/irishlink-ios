1a488ebb986c60021d85cd3291cc26046355db58
William Markley <willjmarkley@gmail.com>
2017-03-02 15:26:46 -0500

    Initial Commit

---
+++ Irish Link/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Irish Link
+//
+//  Created by William Markley on 3/2/17.
+//  Copyright © 2017 William Markley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
